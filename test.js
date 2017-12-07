e62524f8bfc2fd75eab47251604f0239ca915eb9
Badiul Haque Shawon <badiul.haque@enosisbd.com>
2017-12-07 15:14:02 +0600

    #153431053 - Integrate valence SDK

--- test.js
+++ test.js
@@ -2,4 +2,18 @@
 
 const Client = require('./index');
 
-const client = new Client();
+const client = new Client({
+  appId: '31brpbcCLsVim_K4jJ8vzw',
+  appKey: 'sagYSTT_HOts39qrGQTFWA',
+  host: 'https://devcop.brightspace.com',
+  port: 443
+});
+
+client
+  .getVersions()
+  .then(data => {
+    console.log(data);
+  })
+  .catch(err => {
+    console.log(err);
+  });
